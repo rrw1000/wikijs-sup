8ab2faf56ef282beb8c3973fa02928b80de329e7
Richard Watts <rrw@semiramis.org.uk>
2022-08-21 19:02:56 +0100

    "fix" workflows, some terraform.

---
+++ dev/fly/tf/fly.tf
@@ -0,0 +1,25 @@
+# set FLY_API_TOKEN before running.
+# export FLY_API_TOKEN=`flyctl auth token
+
+terraform {
+  required_providers {
+    fly = {
+      source = "fly-apps/fly"
+    }
+  }
+}
+
+provider "fly" {
+}
+
+resource "fly_app" "seSupervisions" {
+  name = "se-supervisions"
+}
+
+resource "fly_volume" "seVolume" {
+  name = "se_supervisions_volume"
+  app = fly_app.seSupervisions.name
+  size = 10
+  region = "lhr"
+  depends_on = [ fly_app.seSupervisions ]
+}
